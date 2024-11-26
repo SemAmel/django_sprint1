50ccbfeb950bbfd124921a43ee1d249eaba7c660
Semyon Amelchenko <semyon.amelchenko@gmail.com>
2024-11-26 12:51:14 +0700

    описал маршруты и view-функции

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def post_detail(request, id):
+    return render(request, 'blog/detail.html')
+
+
+def category_posts(request, category_slug):
+    return render(request, 'blog/category.html')
